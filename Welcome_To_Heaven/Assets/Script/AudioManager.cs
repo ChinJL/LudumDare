6dffa891a89fdaa2c88e0341f891c2fce35eceec
Wai Kit <cwkitkat@gmail.com>
2018-08-14 00:46:52 +0800

    - everything basically

--- Welcome_To_Heaven/Assets/Script/AudioManager.cs
+++ Welcome_To_Heaven/Assets/Script/AudioManager.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AudioManager : MonoBehaviour {
 
 	public static AudioManager audioManager;
 	[SerializeField] private AudioClip[] audios = null;
 	[SerializeField] private AudioSource bgmSource1 = null, bgmSource2 = null, sfxSource = null;
-	private bool isBGM, isSFX;
-	public bool bgm1;
+	private static bool isBGM = true, isSFX = true;
+	[SerializeField] private Image bgmImg = null, sfxImg = null;
+	[SerializeField] private Sprite musicOn = null, musicOff = null, soundOn = null, soundOff = null;
 
 	public enum Sfx{
 		angel, item, win, lose, button
@@ -20,8 +22,50 @@ public class AudioManager : MonoBehaviour {
 		isBGM = true;
 	}
 
+	private void Start(){
+		if (PlayerPrefs.GetFloat ("default") == 0) {
+			PlayerPrefs.SetFloat ("default", 1);
+			PlayerPrefs.SetFloat ("isBGM", 1);
+			PlayerPrefs.SetFloat ("isSFX", 1);
+		}
+		if (PlayerPrefs.GetFloat ("isBGM") == 1) {
+			isBGM = true;
+		} else {
+			isBGM = false;
+		}
+		if (PlayerPrefs.GetFloat ("isSFX") == 1) {
+			isSFX = true;
+		} else {
+			isSFX = false;
+		}
+		if (isBGM) {
+			if (bgmImg != null) {
+				bgmImg.sprite = musicOn;
+			}
+			bgmSource1.volume = 1;
+			bgmSource2.volume = 1;
+		} else {
+			if (bgmImg != null) {
+				bgmImg.sprite = musicOff;
+			}
+			bgmSource1.volume = 0;
+			bgmSource2.volume = 0;
+		}
+		if (isSFX) {
+			if (sfxImg != null) {
+				sfxImg.sprite = soundOn;
+			}
+			sfxSource.volume = 1;
+		} else {
+			if (sfxImg != null) {
+				sfxImg.sprite = soundOff;
+			}
+			sfxSource.volume = 0;
+		}
+	}
+
 	public void PlayMusic(){
-			audioManager.bgmSource1.Play ();
+		audioManager.bgmSource1.Play ();
 	}
 
 	public void PlayMusic2(){
@@ -70,9 +114,13 @@ public class AudioManager : MonoBehaviour {
 	public void ToggleBgm(){
 		isBGM = !isBGM;
 		if (isBGM) {
+			PlayerPrefs.SetFloat ("isBGM", 1);
+			bgmImg.sprite = musicOn;
 			bgmSource1.volume = 1;
 			bgmSource2.volume = 1;
 		} else {
+			PlayerPrefs.SetFloat ("isBGM", 0);
+			bgmImg.sprite = musicOff;
 			bgmSource1.volume = 0;
 			bgmSource2.volume = 0;
 		}
@@ -81,8 +129,12 @@ public class AudioManager : MonoBehaviour {
 	public void ToggleSfx(){
 		isSFX = !isSFX;
 		if (isSFX) {
+			PlayerPrefs.SetFloat ("isSFX", 1);
+			sfxImg.sprite = soundOn;
 			sfxSource.volume = 1;
 		} else {
+			PlayerPrefs.SetFloat ("isSFX", 0);
+			sfxImg.sprite = soundOff;
 			sfxSource.volume = 0;
 		}
 	}
